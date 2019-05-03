93b0672506d1a36b5671eb0e7e5e730249fff1f4
HanBitPix <hanjt517@gmail.com>
2019-05-02 18:04:33 -0700

    Finished Majority of the Project

---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Stock v-for="stock in stocks" v-bind:stock="stock"></Stock>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+import Stock from './Stock.vue';
+
+export default {
+  computed: {
+    ...mapGetters({
+      stocks: 'stockPortfolio'
+    }),
+  },
+  components: {
+    Stock
+  }
+}
+</script>
+
+<style>
+
+</style>
